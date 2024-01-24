ba83394c483e96c6b7d3b2209604c259a76aae6f
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-01-24 18:51:57 -0300

    feature: ngFor

--- Pratica09/src/app/component/wikimedia/wikimedia.component.ts
+++ Pratica09/src/app/component/wikimedia/wikimedia.component.ts
@@ -12,6 +12,7 @@ export class WikimediaComponent {
   extract: string = '';
   valueSearch: string = '';
   listSearch: string[] = [];
+  listabuscas: { titulo: string, result: string }[] = [];
   today = new Date();
 
   @ViewChild('informacaos', { static: true })
@@ -22,8 +23,10 @@ export class WikimediaComponent {
   onsendPages(valor: any) {
     this.valueSearch = valor;
     this.listSearch = [];
+
     this.extract = '';
     this.title = '';
+    this.listabuscas = [];
 
     this.wikipediaService.searchPages(valor).subscribe(
       (dados: any) => {
@@ -31,14 +34,33 @@ export class WikimediaComponent {
 
         if (pages.length > 0) {
           for (let i = 0; i < dados[3].length; i++) {
-            this.listSearch.push(dados[1][i]);
+            // this.listSearch.push(dados[1][i]);
+
+            this.wikipediaService.searchPage(dados[1][i]).subscribe(
+              (dado: any) => {
+                const pages  = (dado as any)['query']['pages']
+                  
+                  Object.keys(pages).forEach((pageId) => {
+                    const page = pages[pageId];
+                    const texto = page.extract;
+                    this.extract = texto;
+                    if(this.extract !== ''){
+                      this.listabuscas.push({ titulo: dados[1][i], result: this.extract });
+                    }
+                      
+                  });
+                },
+                (error) => {
+                  console.error('Erro ao carregar dados:', error);
+                  throw new Error('Resposta de rede não foi bem-sucedida');
+                }
+                );
+              
           }
         } else {
           this.listSearch = []
         }
-
-        // console.log('Extract:', this.listSearch);
-        this.criarElementosBotao(valor, this.listSearch);
+        this.title = " Resultado para busca: "+ valor;       
       },
       (error) => {
         console.error('Erro ao carregar dados:', error);
@@ -46,53 +68,4 @@ export class WikimediaComponent {
       }
     );
   }
-
-  criarElementosBotao(valueSearch: any, returnSearch: string[]): void {
-    const newElementTop = document.createElement('h2');
-    
-    if (this.informacaos.nativeElement.firstChild) {
-      this.informacaos.nativeElement.innerHTML = '';
-    } 
-    if (returnSearch.length == 0 || returnSearch == null || returnSearch == undefined) {
-      newElementTop.innerHTML = ` Não encontrei nada em minha busca com esse o valor "${valueSearch}" `;
-    } else {
-      newElementTop.innerHTML = ` Resultado para busca: "${valueSearch}" `;
-      this.informacaos.nativeElement.appendChild(newElementTop);
-  
-      returnSearch.forEach((result) => {
-        const listItem = document.createElement('h2');
-        listItem.innerHTML = result;
-        listItem.style.cursor = 'pointer';
-        listItem.style.color = "blue";
-        
-
-        this.wikipediaService.searchPage(result).subscribe(
-          (dado: any) => {
-            const pages  = (dado as any)['query']['pages']
-            
-            Object.keys(pages).forEach((pageId) => {
-              const page = pages[pageId];
-              const texto = page.extract;
-             
-              const posicaoPonto = texto.indexOf('.');
-              this.extract = texto.substring(0, posicaoPonto + 1);
-              this.informacaos.nativeElement.appendChild(listItem);
-              const newElementResp = document.createElement('p');
-              newElementResp.innerHTML = this.extract;
-              newElementResp.style.fontStyle = "italic";
-    
-              this.informacaos.nativeElement.appendChild(newElementResp);
-                
-            });
-          },
-          (error) => {
-            console.error('Erro ao carregar dados:', error);
-            throw new Error('Resposta de rede não foi bem-sucedida');
-          }
-        );
-  
-      });
-    }
-  }
-  
 }
