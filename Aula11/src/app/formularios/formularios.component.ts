32fed1bfa7dbca258476dd8296622a2048cbc67c
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2024-02-04 14:35:46 -0300

    validação da idade e conversão para json

--- Aula11/src/app/formularios/formularios.component.ts
+++ Aula11/src/app/formularios/formularios.component.ts
@@ -8,25 +8,31 @@ import { NgForm } from '@angular/forms';
 })
 export class FormulariosComponent {
   @ViewChild('formulario') aviaoForm: NgForm | undefined;
+  menor_idade: boolean = false;
+  dados_formulario: any = '';
 
   onSubmit(){
     if(this.aviaoForm){
       let { nome, senha, email, nome_completo, telefone, endereco, data_nascimento, genero, profissao } = this.aviaoForm.value;
       console.log(nome, senha, email, nome_completo, telefone, endereco, data_nascimento, genero, profissao);
       
+      this.dados_formulario = JSON.stringify(this.aviaoForm.value, null, 2);
+      
       const dataNascimento = new Date(data_nascimento);
       const hoje = new Date();
       const diferencaAnos = hoje.getFullYear() - dataNascimento.getFullYear();
-  
+      
       if (diferencaAnos < 18) {
         console.log('Usuário deve ter no mínimo 18 anos.');
-        return;
+        this.menor_idade = true;
+      }
+      
     }
-
-    }
-
+    
+    console.log(">>>"+this.dados_formulario);
     console.log(this.aviaoForm);
     //this.aviaoForm?.reset();
   }
+
   
 }
