de6839588de5c3ad2412a0dff147046fcef9e341
mikaelleRubia <mikaelle.rubia@outlook.com>
2024-02-04 14:02:03 -0300

    feature: tratamento das labels

--- Aula11/src/app/formularios/formularios.component.ts
+++ Aula11/src/app/formularios/formularios.component.ts
@@ -11,9 +11,20 @@ export class FormulariosComponent {
 
   onSubmit(){
     if(this.aviaoForm){
-      let { login, senha, email } = this.aviaoForm.value;
-      console.log(login, senha, email);
+      let { nome, senha, email, nome_completo, telefone, endereco, data_nascimento, genero, profissao } = this.aviaoForm.value;
+      console.log(nome, senha, email, nome_completo, telefone, endereco, data_nascimento, genero, profissao);
+      
+      const dataNascimento = new Date(data_nascimento);
+      const hoje = new Date();
+      const diferencaAnos = hoje.getFullYear() - dataNascimento.getFullYear();
+  
+      if (diferencaAnos < 18) {
+        console.log('Usuário deve ter no mínimo 18 anos.');
+        return;
     }
+
+    }
+
     console.log(this.aviaoForm);
     //this.aviaoForm?.reset();
   }
